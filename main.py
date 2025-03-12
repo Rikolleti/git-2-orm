4d7638a8913be9a3189a2969835738de1ab041aa
azmamedov <azmamedov@ozon.ru>
2025-03-12 15:28:40 +0300

    Changed backref to backpop

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sqlalchemy
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
@@ -8,18 +7,21 @@ class Publisher(Base):
     __tablename__ = 'publisher'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=60))
+    books = relationship("Book", back_populates="publisher")
 
 class Book(Base):
     __tablename__ = 'book'
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=60))
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"))
-    publisher = relationship("Publisher", backref="book")
+    publisher = relationship("Publisher", back_populates="books")
+    stocks = relationship("Stock", back_populates="book")
 
 class Shop(Base):
     __tablename__ = 'shop'
     id = sq.Column(sq.Integer, primary_key=True)
     name =  sq.Column(sq.String(length=60))
+    stocks = relationship("Stock", back_populates="shop")
 
 class Stock(Base):
     __tablename__ = 'stock'
@@ -27,8 +29,9 @@ class Stock(Base):
     id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"))
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"))
     count =  sq.Column(sq.Integer)
-    book = relationship("Book", backref="stock")
-    shop = relationship("Shop", backref="stock")
+    book = relationship("Book", back_populates="stocks")
+    shop = relationship("Shop", back_populates="stocks")
+    sales = relationship("Sale", back_populates="stock")
 
 class Sale(Base):
     __tablename__ = 'sale'
@@ -37,7 +40,7 @@ class Sale(Base):
     date_sale = sq.Column(sq.DateTime)
     id_stock =  sq.Column(sq.Integer, sq.ForeignKey("stock.id"))
     count = sq.Column(sq.Integer)
-    stock = relationship("Stock", backref="sale")
+    stock = relationship("Stock", back_populates="sales")
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
